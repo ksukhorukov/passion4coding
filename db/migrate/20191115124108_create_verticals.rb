1fcb4161cae109cb52436d3dbcb9c9c38564765b
Kirill Sukhorukov <kirill.suhorukov@gmail.com>
2019-11-15 18:48:12 +0600

    migrations

--- db/migrate/20191115124108_create_verticals.rb
+++ db/migrate/20191115124108_create_verticals.rb
@@ -1,7 +1,7 @@
 class CreateVerticals < ActiveRecord::Migration[6.0]
   def change
     create_table :verticals do |t|
-
+    	t.string :name, null: false
       t.timestamps
     end
   end
--- db/migrate/20191115124140_create_courses.rb
+++ db/migrate/20191115124140_create_courses.rb
@@ -1,7 +1,10 @@
 class CreateCourses < ActiveRecord::Migration[6.0]
   def change
     create_table :courses do |t|
-
+    	t.string :name, null: false
+    	t.string :author, null: false
+    	t.integer :category_id, null: false
+    	t.boolean :state, null: false, default: true
       t.timestamps
     end
   end
