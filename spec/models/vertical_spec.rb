26853b29ce91d4319d2f9c2d32f303b896204976
Kirill Sukhorukov <kirill.suhorukov@gmail.com>
2019-11-16 17:58:58 +0600

    test. vertical have many courses through categories

--- spec/models/vertical_spec.rb
+++ spec/models/vertical_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Vertical, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'assosiations' do 
+  	it { should have_many_to(:categories).class.name == 'Category' }
+  	it { should have_many_through(:courses).class.name == 'Course' }
+  end
+
+  describe 'validations' do 
+  	it { should validate_presence_of(:name) }
+  end
 end
