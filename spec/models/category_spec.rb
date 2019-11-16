cd3ee9183e6105885836dd018ea3732c96cbbb9b
Kirill Sukhorukov <kirill.suhorukov@gmail.com>
2019-11-16 18:10:55 +0600

    tests. unit test for category

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'assosiations' do 
+  	it { should have_many_to(:courses).class.name == 'Course' }
+  	it { should belong_to(:verticals).class.name == 'Vertical' }
+  end
+
+  describe 'validations' do 
+  	it { should validate_presence_of(:name) }
+  	it { should validate_presence_of(:state) }
+  end
 end
