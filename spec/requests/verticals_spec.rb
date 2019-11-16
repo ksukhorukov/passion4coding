a463c2f38bd387894d9ceeea46ef38f4d866dfe8
Kirill Sukhorukov <kirill.suhorukov@gmail.com>
2019-11-16 20:57:41 +0600

    Testing. Verticals integration tests

--- spec/requests/verticals_spec.rb
+++ spec/requests/verticals_spec.rb
@@ -10,4 +10,50 @@ RSpec.describe "Verticals", type: :request do
       expect(JSON.parse(response.body).first["Name"]).to eq(vertical_name)
     end
   end
+
+  describe "POST /verticals" do
+    it "verticals creation" do
+    	vertical_name = FFaker::Lorem.sentence
+    	vertical = Vertical.new(name: vertical_name)
+    	params = vertical.attributes
+      post verticals_path(params)
+      expect(response).to have_http_status(201)
+    end
+  end
+
+  describe "UPDATE /verticals" do
+    it "verticals update" do
+    	vertical_name = FFaker::Lorem.sentence
+    	vertical = Vertical.create(name: vertical_name)
+    	params = vertical.attributes
+    	params[:name] = 'patched name'
+    	patch "/verticals/#{vertical.id}", params: params
+      response.should be_successful
+      expect(JSON.parse(response.body)['Name']) == params[:name]
+    end
+  end
+
+  describe "SHOW /verticals" do
+    it "verticals show" do
+    	vertical_name = FFaker::Lorem.sentence
+    	vertical = Vertical.create(name: vertical_name)
+    	params = vertical.attributes
+    	params[:name] = 'patched name'
+    	get "/verticals/#{vertical.id}"
+      response.should be_successful
+      expect(JSON.parse(response.body)['Name']) == params[:name]
+    end
+  end
+
+  describe "DESTROY /verticals" do
+    it "verticals show" do
+    	vertical_name = FFaker::Lorem.sentence
+    	vertical = Vertical.create(name: vertical_name)
+    	params = vertical.attributes
+    	params[:name] = 'patched name'
+    	delete "/verticals/#{vertical.id}"
+      response.should be_successful
+      expect(Vertical.count) == 0
+    end
+  end
 end
