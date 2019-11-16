02bab1530ba0dd233f7582198cdda9e80ba3f2db
Kirill Sukhorukov <kirill.suhorukov@gmail.com>
2019-11-16 19:52:59 +0600

    testing mail after records creation

--- app/mailers/creation_mailer.rb
+++ app/mailers/creation_mailer.rb
@@ -2,8 +2,8 @@ class CreationMailer < ApplicationMailer
 	default from: ENV['admin_email']
 
   def creation_email
-  	@email = ENV['admin_email']
-    @record = params[:record]
-    mail(to: @email, subject: "#{@record.class.name}. New record was created.")
+  	@to = ENV['admin_email']
+    @record = params.try([:record]) || {}
+    mail(to: @to, subject: "#{@record.class.name}. New record was created.")
   end	
 end
--- spec/mailers/creation_mailer_spec.rb
+++ spec/mailers/creation_mailer_spec.rb
@@ -1,5 +1,11 @@
 require "rails_helper"
 
 RSpec.describe CreationMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+	let(:mail) { CreationMailer.with(record: Vertical.create(name: FFaker::Lorem.sentence)).creation_email }
+
+  describe "notify by email after records creation" do
+    it "sends an email after record creation" do
+      expect(mail.class.name).to eq('ActionMailer::Parameterized::MessageDelivery')
+    end
+  end
 end
