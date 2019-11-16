885b294894784e989d6cf2570777b1b11064713b
Kirill Sukhorukov <kirill.suhorukov@gmail.com>
2019-11-16 21:21:19 +0600

    Testing. Integrations tests for category

--- spec/requests/categories_spec.rb
+++ spec/requests/categories_spec.rb
@@ -2,9 +2,52 @@ require 'rails_helper'
 
 RSpec.describe "Categories", type: :request do
   describe "GET /categories" do
-    it "works! (now write some real specs)" do
+    it "categories index" do
+    	category = FactoryBot.create(:category)
       get categories_path
       expect(response).to have_http_status(200)
+      expect(JSON.parse(response.body).first["Name"]).to eq(category.name)
+    end
+  end
+
+  describe "POST /categorys" do
+    it "categories creation" do
+    	vertical = FactoryBot.create(:vertical)
+      post categories_path, params: { name: 'test category name', state: 'active', vertical_id: vertical.id }
+      expect(response).to have_http_status(201)
+    end
+  end
+
+  describe "UPDATE /categorys" do
+    it "updates categories" do
+    	category = FactoryBot.create(:category)
+    	params = category.attributes
+    	params[:name] = 'patched name'
+    	patch "/categories/#{category.id}", params: params
+      response.should be_successful
+      expect(JSON.parse(response.body)['Name']) == params[:name]
+    end
+  end
+
+  describe "SHOW /categories" do
+    it "categories show" do
+    	category = FactoryBot.create(:category)
+    	params = category.attributes
+    	params[:name] = 'patched name'
+    	get "/categories/#{category.id}"
+      response.should be_successful
+      expect(JSON.parse(response.body)['Name']) == params[:name]
+    end
+  end
+
+  describe "DESTROY /categorys" do
+    it "categories show" do
+			category = FactoryBot.create(:category)
+    	params = category.attributes
+    	params[:name] = 'patched name'
+    	delete "/categories/#{category.id}"
+      response.should be_successful
+      expect(Category.count) == 0
     end
   end
 end
